ac1e550f491b4d035458ecc0548ca978d9a34b9f
Yehor <kravtsovyehor@gmail.com>
2023-02-12 23:17:44 +0200

    Fixed team images

--- src/pages/home/components/team/components/members.jsx
+++ src/pages/home/components/team/components/members.jsx
@@ -10,11 +10,11 @@ export const Members = ({ transform }) => {
       {t.pages.home.team.members.map((member) => (
         <li key={member.id} className="team__item">
           <animated.picture style={{ transform: transform.xy.to(member.trans) }}>
-            <source srcSet={`/img/team/img${member.id}@1x.avif 1x, /img/team/img${member.id}@2x.avif 2x`} type="image/avif" />
-            <source srcSet={`/img/team/img${member.id}@1x.webp 1x, /img/team/img${member.id}@2x.webp 2x`} type="image/webp" />
+            <source srcSet={`img/team/img${member.id}@1x.avif 1x, img/team/img${member.id}@2x.avif 2x`} type="image/avif" />
+            <source srcSet={`img/team/img${member.id}@1x.webp 1x, img/team/img${member.id}@2x.webp 2x`} type="image/webp" />
             <img
-              src={`/img/team/img${member.id}@1x.png`}
-              srcSet={`/img/team/img${member.id}@2x.png 2x`}
+              src={`img/team/img${member.id}@1x.png`}
+              srcSet={`img/team/img${member.id}@2x.png 2x`}
               width="340"
               height="320"
               alt={`Portrait of ${member.name}`}
